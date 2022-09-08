e249e5a478d17b3224c8f9ef7df0be9d75ae1b07
Collin Morudi <collinmorudi@gmail.com>
2022-09-08 21:13:18 +0200

    Fixing the return value of main

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
  * main - prints a simple stirng
- * return - int 0 for success
+ * Return - int 0 for success
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
