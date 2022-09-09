e9fbd0c2405db43c86e4af3cf598be0f49ac4d7b
Collin Morudi <collinmorudi@gmail.com>
2022-09-09 12:06:25 +0200

    initial commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,11 +12,11 @@ int main(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = i+1; j <= '9'; j++)
 		{
 			putchar(i);
 			putchar(j);
-			if (!(i == '9' && j == '9'))
+			if (!(i == '8' && j == '9'))
 			{
 				putchar(',');
 				putchar(' ');
