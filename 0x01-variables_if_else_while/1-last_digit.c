40d6cc7050fb480911e8882f8dae23609ed16c99
Collin Morudi <collinmorudi@gmail.com>
2022-09-12 20:05:27 +0200

    Removing the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int remainder = n % 10;
 
-	if (remainder < 6 && remainder  != 0) 
+	if (n % 10 < 6 && (n % 10) != 0) 
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, remainder);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else if (remainder > 5)
+	else if (n % 10 > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, remainder);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is 0\n", n, remainder);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 
 	return (0);
