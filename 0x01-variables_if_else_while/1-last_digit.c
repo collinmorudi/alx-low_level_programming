d098d047ec4e0d0fa95c71593dd59912e41df9e1
Collin Morudi <collinmorudi@gmail.com>
2022-09-12 20:00:58 +0200

    declaring a new variable and modifyin

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,15 +12,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	int remainder = n % 10;
 
-	if (n < 6 && n != 0) {
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+	if (remainder < 6 && remainder  != 0) 
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, remainder);
 	}
-	else if (n > 5) {
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
+	else if (remainder > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, remainder);
 	}
-	else {
-		printf("Last digit of %d is %d and is 0\n", n, n % 10);
+	else
+	{
+		printf("Last digit of %d is %d and is 0\n", n, remainder);
 	}
 
 	return (0);
