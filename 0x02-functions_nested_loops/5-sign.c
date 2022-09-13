b6fbcb0a314a0e0f669a033e7c3843b504d053f5
Collin Morudi <collinmorudi@gmail.com>
2022-09-13 22:21:16 +0200

    adding implementation to the sign function

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,26 @@
 #include "main.h"
 
+/**
+ * print_sign - prints the sign of some integer
+ * @x: the number to be tested
+ * Return: 1 if greater than 0, 0 if  is equal to 0, and then -1 if less than 0
+ */
 
+int print_sign(int x)
+{
+	if (x == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (x > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
