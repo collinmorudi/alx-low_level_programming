e88daf41409083b86565ce8534bbafbc1b7f0832
Collin Morudi <collinmorudi@gmail.com>
2022-09-13 17:59:56 +0200

    fixing the header file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,5 @@
-#include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
  * main - prints a simple characters to stdio
  * Return: 0 for success
@@ -8,9 +7,9 @@
 
 int main(void)
 {
-	putchar('_');
-	putchar('p');
-	putchar('u');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
 	putchar('t');
 	putchar('c');
 	putchar('h');
