604ee0277b6298b901945e12f0f347e5028329dc
Collin Morudi <collinmorudi@gmail.com>
2022-09-13 21:07:42 +0200

    fixing the bug

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,13 +8,13 @@
 
 int _islower(int c)
 {
-	if (c < 'a' && c > 'z')
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 	_putchar('\n');
 }
