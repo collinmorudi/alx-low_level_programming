5cfd1018ed0164415f1c33608cdbee638e14dbbb
Collin Morudi <collinmorudi@gmail.com>
2022-09-15 10:58:28 +0200

    initial commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints a straight line in the terminal
+ * @n: determines the lenght of the diagonal line
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
