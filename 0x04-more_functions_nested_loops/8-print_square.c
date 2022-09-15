703f9dd5ad725e80f847033a3b9d7a4bad26c057
Collin Morudi <collinmorudi@gmail.com>
2022-09-15 11:35:15 +0200

    initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square in the terminal
+ * @n: determines the lenghth of the square
+ */
+
+void print_square(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+}
