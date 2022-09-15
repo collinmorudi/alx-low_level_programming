a89dc986c1f215a252a0df6933239150d727f954
Collin Morudi <collinmorudi@gmail.com>
2022-09-15 08:32:01 +0200

    Adding a commpleted header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,6 +7,37 @@ int _putchar(char c);
 
 void print_alphabet(void);
 
-/*  */
+/* a function that checks for uppercase character */
+int _isupper(int c);
+
+/* a function that checks for a digit (0 through 9) */
+int _isdigit(int c);
+
+/* a function that multiplies two integers */
+int mul(int a, int b);
+
+/* a function that prints the numbers, from 0 to 9, followed by a new line */
+void print_numbers(void);
+
+/* a function that prints the numbers, from 0 to 9, followed by a new line */
+void print_most_numbers(void);
+
+/* a function that prints 10 times the numbers, from 0 to 14, followed by a new line */
+void more_numbers(void);
+
+/* a function that draws a straight line in the terminal */
+void print_line(int n);
+
+/* a function that draws a diagonal line on the terminal */
+void print_diagonal(int n);
+
+/* a function that prints a square, followed by a new line */
+void print_square(int size);
+
+/* a function that prints a triangle, followed by a new line */
+void print_triangle(int size);
+
+/* a function that prints an integer */
+void print_number(int n);
 
 #endif
