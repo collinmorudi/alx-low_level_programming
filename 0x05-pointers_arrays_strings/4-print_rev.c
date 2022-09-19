f83d433487b6593d34577c9226dab320615abaa8
Collin Morudi <collinmorudi@gmail.com>
2022-09-19 12:12:15 +0200

    initial commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: the string we're working with
+ */
+
+void print_rev(char *s)
+{
+	/* a variable to store length of a string */
+	int len = 0;
+
+	/* compute the length of a string */
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+
+	while (len >= 0)
+	{
+		_putchar(s[len - 1]);
+		len--;
+	}
+
+	_putchar('\n');
+}
