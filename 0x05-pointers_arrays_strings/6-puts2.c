7714ccb8f3cc497e7eefbd95ef1b17db835c5db6
Collin Morudi <collinmorudi@gmail.com>
2022-09-19 15:14:19 +0200

    fixed the bugs

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,17 @@
 void puts2(char *str)
 {
 	/* a variable to store length of a string */
+	/* a variable to store length of a string */
+	int len = 0;
 	int i = 0;
 
-	while (str[i] != '\0')
+	/* compute the length of a string */
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+
+	while (str[i] != '\0' && i < len)
 	{
 		_putchar(*(str + i));
 		i += 2;
