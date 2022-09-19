c4a0d5014f30008f165b68565b3ab358bad7d4f3
Collin Morudi <collinmorudi@gmail.com>
2022-09-19 10:10:58 +0200

    updated a header file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,4 +8,34 @@ int _putchar(char c);
 /* a function that takes a pointer to an int as parameter and updates the value it points to to 98 */
 void reset_to_98(int *n);
 
+/* a function that swaps the values of two integers. */
+void swap_int(int *a, int *b);
+
+/* a function that returns the length of a string. */
+int _strlen(char *s);
+
+/* a function that prints a string, followed by a new line, to stdout. */
+void _puts(char *str);
+
+/* a function that prints a string, in reverse, followed by a new line. */
+void print_rev(char *s);
+
+/* a function that reverses a string. */
+void rev_string(char *s);
+
+/* a function that prints every other character of a string, starting with the first character, followed by a new line. */
+void puts2(char *str);
+
+/* 7. a function that prints half of a string, followed by a new line. */
+void puts_half(char *str);
+
+/* 8. a function that prints n elements of an array of integers, followed by a new line. */
+void print_array(int *a, int n);
+
+/* 9. a function that copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest. */
+char *_strcpy(char *dest, char *src);
+
+/* 10. a function that convert a string to an integer. */
+int _atoi(char *s);
+
 #endif
