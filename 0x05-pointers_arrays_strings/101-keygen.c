b33503e1d27d30a783f0ebff264e68474ed9e6d3
Collin Morudi <collinmorudi@gmail.com>
2022-09-20 05:41:38 +0200

    fixing errors

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -10,16 +10,17 @@ int main(void)
 	int random = 0;
         int count = 0;
 	time_t t;
+	int max = 2500; 
 
 	srand((unsigned int) time(&t));
-	while (count < 3000)
+	while (count < max)
 	{
 		random = rand() % 128;
-		if ((count + random) > 3000)
+		if ((count + random) > max)
 			break;
 		count = count + random;
 		printf("%c", random);
 	}
-	printf("%c\n", (3000 - count));
+	printf("%c\n", (max - count));
 	return (0);
 }
