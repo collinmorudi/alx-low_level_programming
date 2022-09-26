ef2d655a39fc91b0f9f614209dbc01f13e09127e
Collin Morudi <collinmorudi@gmail.com>
2022-09-26 16:09:27 +0200

    initial commit

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy n bytes of memory from source to destination
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: number of bytes to be copied
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+   int size = n;
+   if (size > 0)
+   {
+	int i;
+	for (i = 0; i < size; i++)
+		*(dest + i) = *(src + i);
+   }
+   return (dest);
+}
