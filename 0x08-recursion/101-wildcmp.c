94e07bb085b673a97bd29c686e238306d75ec72d
Collin Morudi <collinmorudi@gmail.com>
2022-09-30 05:58:22 +0200

    initial commit

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -9,21 +9,9 @@
 
 int wildcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0' && s2[j] != '\0')
+	if (s1 == s2)
 	{
-		if (s1[i] == s2[j])
-			i++, j++;
-		if (s2[j] == '*')
-		{
-			j++;
-			while ((s1[i] != s2[j]) && s1[i] != '\0')
-				i++;
-		}
-		if (s1[i] != s2[j])
-			return (0);
+		return (1);
 	}
 	return (1);
 }
