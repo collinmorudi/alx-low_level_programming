6f6756292dae15a5878a55eaccd12a4bd4e7f8fd
Collin Morudi <collinmorudi@gmail.com>
2022-10-04 19:37:28 +0200

    adding parentheses on return statements

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,16 +15,12 @@ char *create_array(unsigned int size, char c)
 
 	/* retun null if size = 0 */
 	if (size == 0)
-	{
-		return NULL;
-	}
+		return (NULL);
 
 	ptr = (char *)malloc(size * sizeof(char));
 
 	if (ptr == NULL)
-	{
-		return NULL;
-	}
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
