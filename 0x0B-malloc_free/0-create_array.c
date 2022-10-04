d73474e638d217b45b19bc9dcf36dff3358095d2
Collin Morudi <collinmorudi@gmail.com>
2022-10-04 19:32:10 +0200

    Adding a condition to check for Null

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,16 +11,21 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-        char *ptr = (char *)malloc(size * sizeof(char));
+	char *ptr;
+
+	/* retun null if size = 0 */
+	if (size == 0)
+		return NULL;
+
+	ptr = (char *)malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return NULL;
 
 	for (i = 0; i < size; i++)
 	{
 		*(ptr + i) = c;
 	}
-
-	/* retun null if size = 0 */
-	if (size == 0)
-		return NULL;
 
 	return ptr;
 }
