188eff82090e2d2d5835ee642ebfa3d7fab722a1
Collin Morudi <collinmorudi@gmail.com>
2022-10-05 14:55:27 +0200

    initial commit

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,5 +1,9 @@
 #include "main.h"
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _strlen(char *s);
 
 /**
  * argstostr - add a desc
@@ -10,7 +14,52 @@
 
 char *argstostr(int ac, char **av)
 {
-        if (ac == NULL || av == NULL)
-                return (NULL);
-        return (NULL);
+	int i, j, len = 0, k = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+
+	len += (ac + 1);
+
+	str = (char *)malloc(sizeof(char));
+
+	if (str == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			str[k++] = av[i][j];
+		}
+
+		str[k++] = '\n';
+	}
+
+	return (str);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
 }
