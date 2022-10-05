a706bcc5bdc6695bfdf81a5f9e4ca808c6c31bc2
Collin Morudi <collinmorudi@gmail.com>
2022-10-05 11:27:55 +0200

    ninitializing the variables

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,15 +13,21 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *dup_string;
-	int len1, len2, i = 0;
+	int len1 = 0, len2 = 0, i = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	/*get length of string s1*/
-	while(*(s1 + i))
+	while(*(s1 + i) != '\0')
 		len1++, i++;
 
 	/*get length of string s2*/
 	i = 0;
-	while(*(s2 + i))
+	while(*(s2 + i) != '\0')
 		len2++, i++;
 
 	len2++; /* increment len2 to acommodate null terminator */
