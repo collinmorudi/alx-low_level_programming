6cf346fafcda91ebe26a240680190d5a24e9a0f8
Collin Morudi <collinmorudi@gmail.com>
2022-10-05 05:47:18 +0200

    inittial commit

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * strtow - add a desc
+ * @str: a string to work with
+ * Return: a pointer
+ */
+
+char **strtow(char *str)
+{
+	if (str == NULL or str == "")
+		return (Null);
+	return (NULL);
+}
