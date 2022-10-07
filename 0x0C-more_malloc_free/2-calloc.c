afed7ef113549e3d67058c9075ca57ed293c0569
Collin Morudi <collinmorudi@gmail.com>
2022-10-07 03:12:18 +0200

    initial commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocate memory and make all values to 0
+ * @nmemb: size of the ...
+ * @size: size of ...
+ * Return: pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *p;
+	unsigned int i;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		*((char *)p + i) = 0;
+
+	return (p);
+}
