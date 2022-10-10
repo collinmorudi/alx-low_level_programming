df0b0e14587b55c3e69883d5f47a40e9dff82369
Collin Morudi <collinmorudi@gmail.com>
2022-10-10 20:29:42 +0200

    initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog - This is a dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: name of the dog's owner
+ */
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+/* a function that initialize a variable of type struct dog */
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+/*  a function that prints a struct dog */
+void print_dog(struct dog *d);
+
+/* a function that creates a new dog. */
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
