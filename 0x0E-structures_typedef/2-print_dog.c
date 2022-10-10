f285b67cbe20f3cc5178e7958be7ffcd4cc7822c
Collin Morudi <collinmorudi@gmail.com>
2022-10-10 21:17:55 +0200

    initial commit

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints the props of the dog struct
+ * @d: the dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)");
+		return;
+	}
+
+	if (d->owner == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+
+	/* print the dog properties */
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
