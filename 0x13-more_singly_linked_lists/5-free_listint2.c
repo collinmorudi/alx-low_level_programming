f6816c4f076c79adcb075eca38ab1d6ee45b12dd
Collin Morudi <collinmorudi@gmail.com>
2022-11-17 11:02:06 +0200

    Initial commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a linked list.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *current;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((tmp = current) != NULL)
+		{
+			current = current->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+}
