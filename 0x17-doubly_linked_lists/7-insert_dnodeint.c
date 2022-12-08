ed85550a4a54eb9e050c8d202dbabf2ce718b5eb
Collin Morudi <collinmorudi@gmail.com>
2022-12-08 18:07:37 +0200

    initial commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,49 +1,51 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node at index of a list
- * @head: head node of the list
- * @index: index of the new node in a list
- * Return: 1 if it successful, -1 otherwise
+ * insert_dnodeint_at_index - inserts a new node at the idx
+ * @h: head node of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if unsuccessful
  */
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *h1, *h2;
+	dlistint_t *new_node, *head;
 	unsigned int i;
 
-	h1 = *head;
-
-	if (h1 != NULL)
-		while (h1->prev != NULL)
-			h1 = h1->prev;
-
-	i = 0;
-
-	while (h1 != NULL)
+	new_node = NULL;
+	if (idx == 0)
+		new_node = add_dnodeint(h, n);
+	else
 	{
-		if (i == index)
+		head = *h;
+		i = 1;
+		if (head != NULL)
+			while (head->prev != NULL)
+				head = head->prev;
+		while (head != NULL)
 		{
-			if (i == 0)
+			if (i == idx)
 			{
-				*head = h1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
+				if (head->next == NULL)
+					new_node = add_dnodeint_end(h, n);
+				else
+				{
+					new_node = malloc(sizeof(dlistint_t));
+					if (new_node != NULL)
+					{
+						new_node->n = n;
+						new_node->next = head->next;
+						new_node->prev = head;
+						head->next->prev = new_node;
+						head->next = new_node;
+					}
+				}
+				break;
 			}
-			else
-			{
-				h2->next = h1->next;
-
-				if (h1->next != NULL)
-					h1->next->prev = h2;
-			}
-
-			free(h1);
-			return (1);
+			head = head->next;
+			i++;
 		}
-		h2 = h1;
-		h1 = h1->next;
-		i++;
 	}
 
-	return (-1);
+	return (new_node);
 }
