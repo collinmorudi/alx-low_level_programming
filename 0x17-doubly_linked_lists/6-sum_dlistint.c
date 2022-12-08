00f3f9390e311c0d378255120732ddc752926c1b
Collin Morudi <collinmorudi@gmail.com>
2022-12-08 17:48:19 +0200

    initial commit

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data in a list
+ * @head: head node of the list
+ * Return: sum of the data in the list
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head != NULL)
+	{
+		while (head->prev != NULL)
+			head = head->prev;
+
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+	}
+
+	return (sum);
+}
