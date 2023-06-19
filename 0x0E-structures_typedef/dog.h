a28421953e68fb037b9996d296be79f40da29b9e
Collin Morudi <collinmorudi@gmail.com>
2023-06-19 15:18:52 +0200

    Initial commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * dog_t - Typedef for the struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - structure for dogs
+ * @name: 1st member
+ * @age: 2nd member
+ * @owner: 3rd member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
