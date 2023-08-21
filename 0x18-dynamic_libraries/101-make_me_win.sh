1474b7b29ec2c2b657194fb184c48c6b1124bc21
Collin Morudi <collinmorudi@gmail.com>
2023-08-21 12:07:49 +0200

    fixing errors

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/collinmorudi/alx-low_level_programming/master/0x18-dynamic_libraries/random.so
-export LD_PRELOAD=random.so
+wget -P /tmp/ https://github.com/collinmorudi/alx-low_level_programming/blob/master/0x18-dynamic_libraries/random.so
+export LD_PRELOAD=/tmp/random.so
